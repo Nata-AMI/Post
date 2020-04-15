10b52f138a74761881b57c7092d5b4a8fc65a211
Natalia Bryzgalova <bryzgalova_n@mail.ru>
2020-04-15 21:33:08 +0500

    OverWork2 is done

--- src/main/java/manager/PostManager.java
+++ src/main/java/manager/PostManager.java
@@ -3,19 +3,13 @@ package manager;
 import domain.*;
 
 public class PostManager {
-    private int ownerId;
-    private String domain;
-    private String query;
-    private boolean ownersOnly;
-    private int postId;
-    private  int count;
-    private int offset;
+
 
     public Post[] wallSearch(int ownerId, String domain, String query, boolean ownersOnly, int count, int offset) {
         return null;
     }
 
-    public Post[] wallDelete(int ownerId, int postId) {
+    public void wallDelete(int ownerId, int postId) {
     }
 
 }
